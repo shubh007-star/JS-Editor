6d1dc8231649efb9e3bebb2251aab4fb385d0e05
Siddharth <siddharthsingh330@yahoo.in>
2021-03-19 11:54:42 +0530

    Updated JS IDE Project

--- index.js
+++ index.js
@@ -1,39 +1,95 @@
-$("a").attr("href", "https://www.bing.com");
-alert("Things are about to be changed");
+// initialize the content of the text editor to some Javascript
+$("#editor").text(`function echo(m) {\n\treturn m;\n}\nconsole.log(echo("Hello World"));`);
 
-$("h1").text("JS Code Editor");
-
-var x = true;
-$("button").click(function () {
-  if (x == true) {
-    $("h1").css("color", "#9b3675");
-    x = false;
-  } else {
-    $("h1").css("color", "#ff7171");
-    x = true;
-  }
+// initialize the editor environment using the CodeMirror library
+//refer CodeMirror documentation
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+  mode: "javascript",
+  theme: "dracula",
+  lineNumbers: true,
+  smartIndent: true,
+  matchBrackets: true,
+  closebrackets: true,
 });
-
-//For executing the code
-$(".btn").click(function () {
-  var js = editor.getValue(); //http://codemirror.net/doc/manual.html#getValue
-  var s = document.createElement("script");
-  s.textContent = js; //inne
-  document.body.appendChild(s);
-});
+editor.setSize("1366", "600");
 
 
-//O/P handeling console.log
-(function () {
-    var old = console.log;
-    var logger = document.getElementById('log');
-    console.log = function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (typeof arguments[i] == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
-        } else {
-            logger.innerHTML += arguments[i] + '<br />';
-        }
+// Override default console functions for our custom Dev Console
+// in order to print all our outputs in our integrated console instead of the chrome dev tools console
+(function() {
+  // store default console functionality before changing them
+  default_log = console.log;
+  default_clear = console.clear;
+  default_error = console.error;
+  default_warn = console.warn;
+
+  console.log = function(...args) {
+    for (let arg of args) {
+      if (typeof arg == 'object') {
+        $("#console").append((JSON && JSON.stringify ? JSON.stringify(arg, undefined, 2) : arg) + ' ');
+      } else {
+        $("#console").append(arg + ' ');
       }
     }
+    // Console prompt
+    $("#console").append('\n&raquo;  ');
+
+    // So console is always scrolled to the bottom
+    $("#console").get(0).scrollTop = $("#console").get(0).scrollHeight;
+
+    // Allow the default console action to happen
+    default_log(...args)
+  }
+  console.error = function(e) {
+
+    $("#console").append("Error: " + e);
+
+    // Console prompt
+    $("#console").append('\n&raquo;  ');
+
+    // So console is always scrolled to the bottom
+    $("#console").get(0).scrollTop = $("#console").get(0).scrollHeight;
+
+    // Allow the default console action to happen
+    default_error(e)
+  }
+  console.warn = function(w) {
+    $("#console").append("Warning: " + w);
+
+    // Console prompt
+    $("#console").append('\n&raquo;  ');
+
+    // So console is always scrolled to the bottom
+    $("#console").get(0).scrollTop = $("#console").get(0).scrollHeight;
+
+    // Allow the default console action to happen
+    default_warn(w)
+  }
+  console.clear = function() {
+    // Console prompt
+    $("#console").html("&raquo;  ");
+    // Allow the default console action to happen
+    default_clear();
+  }
+  clear = console.clear;
 })();
+
+// For our custom clear and execute shortcuts Ctrl-Enter and Ctrl-I
+function kbd(e) {
+  if (e.key === "i") console.clear();
+  if (e.key === "Enter") eval(editor.getValue());
+}
+
+window.addEventListener('keydown', function(e) {
+  if (e.key === "Control") window.addEventListener('keydown', kbd);
+});
+window.addEventListener('keyup', function(e) {
+  if (e.key === "Control") window.removeEventListener('keydown', kbd);
+})
+// For phones and devices without a control key
+$("[action]").get(0).addEventListener('click', function() {
+  eval(editor.getValue());
+});
+$("[action]").get(1).addEventListener('click', function() {
+  console.clear();
+});
