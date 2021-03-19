f1a0926cc42846a8e1763a0740859247987cf1f3
Siddharth Singh <62372310+SIDTK365@users.noreply.github.com>
2021-03-19 14:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   autoCloseBrackets: true,
   showCursorWhenSelecting: true,
   keyMap: "sublime",
+  
+  //For autocomplete more, more code at the bottom 
+  extraKeys: {"Ctrl-Space": "autocomplete"},
+  mode: {name: "javascript", globalVars: true},
+  
 });
 editor.setSize("100%", "100%");
 
@@ -96,3 +101,44 @@ $("[action]").get(0).addEventListener('click', function() {
 $("[action]").get(1).addEventListener('click', function() {
   console.clear();
 });
+
+
+//JS autocomplete documentation @Codemirror autocomplete feature
+if (typeof Promise !== "undefined") {
+  var comp = [
+    ["here", "hither"],
+    ["asynchronous", "nonsynchronous"],
+    ["completion", "achievement", "conclusion", "culmination", "expirations"],
+    ["hinting", "advise", "broach", "imply"],
+    ["function","action"],
+    ["provide", "add", "bring", "give"],
+    ["synonyms", "equivalents"],
+    ["words", "token"],
+    ["each", "every"],
+  ]
+
+  function synonyms(cm, option) {
+    return new Promise(function(accept) {
+      setTimeout(function() {
+        var cursor = cm.getCursor(), line = cm.getLine(cursor.line)
+        var start = cursor.ch, end = cursor.ch
+        while (start && /\w/.test(line.charAt(start - 1))) --start
+        while (end < line.length && /\w/.test(line.charAt(end))) ++end
+        var word = line.slice(start, end).toLowerCase()
+        for (var i = 0; i < comp.length; i++) if (comp[i].indexOf(word) != -1)
+          return accept({list: comp[i],
+                         from: CodeMirror.Pos(cursor.line, start),
+                         to: CodeMirror.Pos(cursor.line, end)})
+        return accept(null)
+      }, 100)
+    })
+  }
+
+  var editor2 = CodeMirror.fromTextArea(document.getElementById("synonyms"), {
+    extraKeys: {"Ctrl-Space": "autocomplete"},
+    lineNumbers: true,
+    lineWrapping: true,
+    mode: "text/x-markdown",
+    hintOptions: {hint: synonyms}
+  })
+}
